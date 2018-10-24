767ff6e12d44dbe815ec49539459b659cc769ef9
emcbride09 <elliot.a.mcbride@gmail.com>
2018-10-24 12:46:48 +0100

    new plot

--- partner_plot.R
+++ partner_plot.R
@@ -2,6 +2,10 @@ install.packages("ggrepel")
 require(ggrepel)
 require(tidyverse)
 require(googlesheets)
+devtools::install_github("hrbrmstr/ggalt")
+library(ggplot2)
+library(ggalt)
+library(ggfortify)
 
 org_data <- gs_title("Organisation Mapping and Assessment")
 
@@ -15,28 +19,41 @@ org_sheet$org_tier <- as.factor(org_sheet$org_tier)
 org_sheet$data_quant <- as.numeric(org_sheet$data_quant)
 org_sheet$data_relevance <- as.numeric(org_sheet$data_relevance)
 
-org_graph <- org_sheet %>% ggplot(aes(data_relevance, data_quant, label = org_name, color = org_tier)) + 
-  labs(title = "EAA Partnership Organisation Map", x = "Data Relevance to EAA", y = "Data Quantity", 
-       subtitle = "EAA Partnership Targets")
+org_graph <- org_sheet %>% ggplot(aes(data_relevance, data_availability, label = org_name, color = org_tier)) + 
+  labs(title = "EAA Partnership Organisation Map", x = "Data Relevance to EAA", y = "Data Availability", 
+       subtitle = "EAA 2nd Year Partnership Targets")
 
 org_graph + geom_point(size = 3) + 
   xlim(0,10) +
   ylim(0,10) +
-  geom_label_repel(nudge_x = 0.55, nudge_y = 0.2, show.legend = FALSE) +
+  geom_label_repel(nudge_x = 0.55, nudge_y = 0.2, show.legend = FALSE,
+                   label.size = .5,
+                   box.padding = 0.5,
+                   force = 2) +
+  geom_encircle() +
   scale_color_brewer(palette = "Dark2", name = "Organisation Tier",
                      labels = c("Priority", "Desirable", "Secondary")) +
   theme(plot.title = element_text(family = 'Arial', 
-                                        size = 16, 
+                                        size = 30, 
                                         vjust = 0,
                                         hjust = 0,
                                         colour = 'black'),
           plot.subtitle = element_text(family = 'Arial',
-                                       size = 10),
+                                       size = 16),
         panel.background = element_rect(fill = 'white'),
         plot.background = element_rect(fill = 'white'),
         panel.grid = element_blank(),
         legend.key = element_rect(fill = NA, size = 7, color = NA),
-        legend.background = element_rect(fill = 'white'))
+        legend.background = element_rect(fill = 'white'),
+        legend.text = element_text(size = 14),
+        legend.title = element_text(size = 14))
+
+ggsave("partnership_orgs.png", plot = last_plot(), width = 250, height = 150, units = "mm", dpi = 400)
+
+
+bar <- geom
+
+
 
 
                      
